3bfa520f0897c24213e4e027ed135b829803a643
TIAGO FERREIRA <tiago.vbsa@gmail.com>
2019-03-21 10:45:16 -0300

    Atualização de Código Celso

--- src/test/java/curso/treinamento/runners/RunnerTest.java
+++ src/test/java/curso/treinamento/runners/RunnerTest.java
@@ -1,5 +1,18 @@
 package curso.treinamento.runners;
 
-public class RunnerTest {
+import org.junit.runner.RunWith;
 
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = {"src\\test\\resources\\features\\login.feature"},
+		glue = {"curso.treinamento.steps"},
+		snippets = SnippetType.CAMELCASE,
+		tags = {})
+
+public class RunnerTest {
+	
 }
---
+++ src/test/java/curso/treinamento/steps/LoginSteps.java
@@ -0,0 +1,47 @@
+package curso.treinamento.steps;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.Então;
+import cucumber.api.java.pt.Quando;
+
+public class LoginSteps {
+
+	@Dado("^que eu esteja na tela de login$")
+	public void queEuEstejaNaTelaDeLogin() {
+		
+		//Setando o local onde está o driver do Google Chrome 
+		System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\drivers\\chromedriver.exe");
+		
+		// Criando um objeto do Navegador
+		WebDriver driver = new ChromeDriver();
+		
+		// Passando a URL que desejamos abrir 
+		driver.get("http://newtours.demoaut.com/");
+		
+		// Preenchendo o elemendo de User
+		driver.manage().window().maximize();	
+		driver.findElement(By.name("userName")).sendKeys("mercury");
+		driver.findElement(By.name("password")).sendKeys("mercury");
+		driver.findElement(By.name("login")).click();
+		driver.quit();
+		
+	//	WebDriverWait wait = new WebDriverWait(driver, 20);
+		
+		
+		
+	}
+
+	@Quando("^faço login com o usuário \"([^\"]*)\" e senha \"([^\"]*)\" ​$")
+	public void façoLoginComOUsuárioESenha​(String arg1, String arg2) {
+	}
+
+	@Então("^sou autenticado com sucesso$")
+	public void souAutenticadoComSucesso() {
+	}
+
+}
